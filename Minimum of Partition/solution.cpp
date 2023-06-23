655b25a33b75f6dfd86f86dc89b4f6eceada072f
ndemarch <73549112+ndemarch@users.noreply.github.com>
2023-06-23 17:09:41 -0400

    Create solution.cpp

---
+++ Minimum of Partition/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findValueOfPartition(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        long long min = 10e9;
+        long long difference = 10e9 - 1;
+        for (int i=1; i<nums.size(); i++){
+            difference = nums[i] - nums[i-1];
+            if(difference < min){
+                min = difference;
+            }
+        }
+        return min;
+    }
+};
