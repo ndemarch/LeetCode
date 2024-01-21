00d396c669611d845bfbdfe752fb32a259ded510
ndemarch <73549112+ndemarch@users.noreply.github.com>
2024-01-21 17:38:02 -0500

    Create  solution.cpp

---
+++ House Robber/solution.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = size(nums);
+        if (n == 0){
+            return 0;
+        }
+        else if (n == 1){
+            return nums[0];
+        }
+        int first = nums[0];
+        int second = max(nums[0], nums[1]);
+        for (int i = 2; i < n; i++){
+            int currentSum = max(second, first + nums[i]);
+            first = secon;
+            second = currentSum;
+        }
+        return second;
+    }
+};
